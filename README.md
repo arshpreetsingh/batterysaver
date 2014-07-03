3dce4a242a48086a0b436da15e3b261384de81db
honey <honey@honey.(none)>
2014-07-04 02:05:55 +0530

    readme added and tinkring with acpi-module.py

---
+++ README.md
@@ -0,0 +1,29 @@
+PythonBatterySaver
+============
+
+##INTRODUCTION##
+
+Pyhton Battery Saver Pack is completly dedicated to Prolong Lithium-based Batteries. This is very accidental. It helps to improve battry life
+almost 1.5 Times. :)
+
+##MOTIVATION##
+
+Thanks to Dr. HS Rai for showing us path to this unique Idea and believing in us throughout the whole process.
+Free Software: http://hsrai.wordpress.com/
+
+##INSPIRATION##
+
+It is based on the Article provided by Battery University:
+http://batteryuniversity.com/learn/article/how_to_prolong_lithium_based_batteries
+
+##LEAD DEVELOPERS##
+Sandeep Kaur
+Email:mkaurkhalsa@gmail.com
+
+Arshpreet Singh
+Email:arsh840@gmail.com
+
+##FUTURE DEVELOPMENT##
+
+This version is only for x86-32 and x86-64 bit architecture (Mostly All INTEL and AMD processors) but we believe every device which keeps 
+battery for Main or backup power must have this application.
---
+++ acpi-install.py
@@ -0,0 +1,53 @@
+"""
+%%% acpi-install.py %%%
+This file is used to check whether the system has acpi package or not. If it 
+does not fine the package, it is installed.
+"""
+
+########################## Header Files ########################################
+import os
+from sys import platform as _platform
+import platform
+import urllib2
+################################################################################
+
+def check_connectivity():
+	"""This function checks the connectivity to internet"""
+	try:
+		urllib2.urlopen('http://www.google.com',timeout=2)
+		return True
+	except urllib.request.URLError:
+		print("No internet connection available. Please get one to move further")
+		return False
+        
+def install_acpi():
+	"""We find the distribuion of the system and then run command to install 
+	acpi if it is not installed"""
+	check_connectivity()
+	linux_distro = platform.linux_distribution()[0]
+
+	try:
+		__import__('acpi')
+	except ImportError:
+		if _platform == "linux" or _platform == "linux2":
+			if linux_distro=='Ubuntu':
+				os.system("sudo apt-get install acpi -y")
+				os.system("sudo apt-get install python-pip -y")
+				os.system("sudo pip install acpi")
+			elif linux_distro=='RedHat':
+				os.system("sudo yum -y install acpi")
+				os.system("sudo yum -y install python-pip")
+		
+		# linux
+		elif _platform == "darwin":
+			pass
+			#here we need a command to install acpi on darwin
+			# OS X
+		elif _platform == "win32":
+			#here we need a command to install acpi on window's DOS	
+			pass
+			
+		print "Ahaa..!! acpi application+module are installed successfully"
+	
+install_acpi();
+    
